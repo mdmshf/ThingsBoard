9d42830a071edfd63464910e687a21d189b9b11d
mdmshf <31364311+mdmshf@users.noreply.github.com>
2017-11-13 22:29:00 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class HelloWorld
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		System.out.println("Hello, World");         // Prints "Hello, World" on output screen.
+	}
+}
