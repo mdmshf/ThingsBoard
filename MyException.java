e3d206cbbc74a89a6d973abe9620c254e858ea9e
Farhan ul Haque <ffarhan99@gmail.com>
2017-11-16 01:17:09 +0530

    Update MyException.java

--- MyException.java
+++ MyException.java
@@ -3,54 +3,82 @@ import java.util.*;
 
 class MyException {
 
-public static void main(String[] args){
+public static void main(String[] args)throws Exception
+{
 
-Scanner sc=new Scanner(System.in);
+	Scanner sc=new Scanner(System.in);
 
-int age;
-age=sc.nextInt(); 
+	int age=0;
 
 try
 {
+	age=sc.nextInt(); 
+}
+
+catch(Exception e)
+{
+        System.out.println("Please enter correct input");
+        System.out.println(e.getMessage());
+        e.printStackTrace();
+
+}
+      
 
 	if (age < 18)
-	{
+{
 		throw new TooyoungException("You are too young to vote");
 }
 
+
 	else if (age >= 60)
-	 {
+ {
 		throw new OldException("You are an eligible Senior Citizen voter");
 }
 
 	else
-	{
+{
 		System.out.println("You are an eligible voter");
 }
 		}
 
-catch(Exception e)
-{
-	System.out.println("Please enter correct input");
-	System.out.println(e.getMessage());
-	//System.out.println(e.printStackTrace());
-
-	}
-		}
+		
 }
 
 class TooyoungException extends Exception 
 {
 
- TooyoungException(String s) {
-  super(s);
-		}
-	}
+ TooyoungException(String s)
+ 	{
+ 		 super(s);
+				}
+}
 
 
 class OldException extends Exception
 {
-OldException(String s) {
-super(s);
+	OldException(String s)
+	{
+	
+		super(s);
  }
-	}
+		}
+
+
+class TooyoungException extends Exception 
+{
+
+ 	TooyoungException(String s)
+	{
+  		super(s);
+			
+			}
+				}
+
+
+class OldException extends Exception
+{
+OldException(String s) 
+	{
+		super(s);
+ }
+			}
